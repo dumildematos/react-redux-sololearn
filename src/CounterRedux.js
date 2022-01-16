e9f5dbc86b979e74490a27730b11cd3d08a4f0ff
Dumilde Matos <dumildejosematos@gmail.com>
2022-01-16 08:04:26 +0100

    add redux

---
+++ src/CounterRedux.js
@@ -0,0 +1,113 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import Hello from './Hello';
+// import Converter from './Converter';
+// import AddForm from './AddForm';
+// import MyList from './MyList';
+// import ContactList from './ContactList/ContactList';
+// import Counter from './Counter';
+// import CounterRedux from './CounterRedux';
+// // class Hello extends React.Component {
+// //   render() {
+// //     return <h1>Hello, {this.props.name}!</h1>;
+// //   }
+// // }
+
+
+// // class Hello extends React.Component {
+
+// //   state = {
+// //       name: 'James',
+// //   }
+
+// //   render() {
+// //       return <h1>Hello {this.state.name} !</h1>
+// //   }
+// // }
+
+// // class Counter extends React.Component {
+
+// //   state = {
+// //     count: 0
+// //   }
+
+// //   increment = () => {
+
+// //     this.setState({
+// //       count: this.state.count+1
+// //     })
+
+// //   }
+
+// //   componentDidMount  (){
+// //     this.setState({count: 42})
+// //   }
+
+// //   componentDidUpdate (){
+// //     alert("Number of clicks: " + this.state.count)
+// //   }
+
+// //   render() {
+// //     return <div>
+// //       <p> { this.state.count } </p>
+// //       <button onClick={this.increment}>Increment</button>
+// //     </div>
+// //   } 
+
+// // }
+
+// const arr = ['A','B','C'];
+
+
+
+// ReactDOM.render(
+//   <div>
+//       <CounterRedux />
+//   </div>
+//   ,
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// function Item(props){
+//   return <div class="item">
+//     <b>Name </b>: {props.name} <br/>
+//     <b>Price</b>: {props.price} $<br/>
+//   </div>
+// }
+
+// // // If you want to start measuring performance in your app, pass a function
+// // // to log results (for example: reportWebVitals(console.log))
+// // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+
+// // function Hello(props) {
+// //   return <h1>Hello, {props.name}!</h1>;
+// // }
+
+
+
+// // let counter = 0;
+
+// // function show(){
+
+// //   counter++;
+// //   const el = <p>{ counter }</p>
+// //   ReactDOM.render(
+// //     el,
+// //     document.getElementById('root')
+// //   )
+
+// // }
+
+// // setInterval(show, 1000);
+
+
--- src/index.js
+++ src/index.js
@@ -1,103 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Hello from './Hello';
-import Converter from './Converter';
-import AddForm from './AddForm';
-import MyList from './MyList';
-import ContactList from './ContactList/ContactList';
-// class Hello extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}!</h1>;
-//   }
-// }
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Counter from './Counter';
 
-
-// class Hello extends React.Component {
-
-//   state = {
-//       name: 'James',
-//   }
-
-//   render() {
-//       return <h1>Hello {this.state.name} !</h1>
-//   }
-// }
-
-class Counter extends React.Component {
-
-  state = {
-    count: 0
+const initialState = {
+  count: 0
+};
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    default:
+      return state;
   }
-
-  increment = () => {
-
-    this.setState({
-      count: this.state.count+1
-    })
-
-  }
-
-  componentDidMount  (){
-    this.setState({count: 42})
-  }
-
-  componentDidUpdate (){
-    alert("Number of clicks: " + this.state.count)
-  }
-
-  render() {
-    return <div>
-      <p> { this.state.count } </p>
-      <button onClick={this.increment}>Increment</button>
-    </div>
-  } 
-
 }
-const arr = ['A','B','C'];
+
+const store = createStore(reducer);
+const el = <Provider store={store}>
+          <Counter/>
+        </Provider>; 
+
 ReactDOM.render(
-  <div>
-    <ContactList />
-  </div>
-  ,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
+  el, 
   document.getElementById('root')
 );
-
-
-function Item(props){
-  return <div class="item">
-    <b>Name </b>: {props.name} <br/>
-    <b>Price</b>: {props.price} $<br/>
-  </div>
-}
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-// function Hello(props) {
-//   return <h1>Hello, {props.name}!</h1>;
-// }
-
-
-
-// let counter = 0;
-
-// function show(){
-
-//   counter++;
-//   const el = <p>{ counter }</p>
-//   ReactDOM.render(
-//     el,
-//     document.getElementById('root')
-//   )
-
-// }
-
-// setInterval(show, 1000);
